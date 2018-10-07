4acff5e74e40898578a121bd2a1b6984a59cfa39
Marcin Lasak <lasakmarcin90@gmail.com>
2018-10-07 13:23:03 +0200

    Introduce some html optimizations

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const renderPage = require('./puppeteer/renderPage');
+const htmlOptimizer = require('./htmlOptimizer');
 
 app.get('/', async (req, res) => {
     const resultHTML = await renderPage(req.query.url);
-    res.send(resultHTML);
+    const optimizedHTML = htmlOptimizer(resultHTML, true);
+    res.send(optimizedHTML);
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
